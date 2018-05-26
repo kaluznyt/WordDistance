a4f5589cb1d63a33d8883f4c8c9b98d7337dc942
Tomasz Kaluzny <tkaluzny@outlook.com>
2018-05-26 12:10:46 +0100

    Initial commit, WordDistanceCalculator + Tests

---
+++ WordDistance.Tests/WordDistanceCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace WordDistance.Tests
+{
+    public class WordDistanceCalculator
+    {
+        public int Calculate(string startWord, string endWord)
+        {
+            if (string.IsNullOrEmpty(startWord) || string.IsNullOrEmpty(endWord))
+            {
+                throw new ArgumentException($"Parameters {nameof(startWord)},{nameof(endWord)} should not be empty");
+            }
+
+            if (startWord.Length != endWord.Length)
+            {
+                throw new ArgumentException($"{nameof(startWord)} and {nameof(endWord)} should have the same length");
+            }
+
+            return startWord.Select((character, characterIndex) => character != endWord[characterIndex] ? 1 : 0)
+                            .Sum();
+        }
+    }
+}
+
+
---
+++ WordDistance.Tests/WordDistanceCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+
+namespace WordDistance.Tests
+{
+    public class WordDistanceCalculatorTests
+    {
+        [Theory]
+        [InlineData("spin", "spot", 2)]
+        [InlineData("spina", "spotd", 3)]
+        [InlineData("abce", "bcde", 3)]
+        [InlineData("tomasz", "tomasx", 1)]
+        [InlineData("tombsz", "tomasx", 2)]
+        [InlineData("aombsz", "tomasx", 3)]
+        public void Calculate_Word_Distance_Returns_Shortest_Path(string startWord, string endWord, int expectedDistance)
+        {
+            var wordDistanceCalculator = new WordDistanceCalculator();
+
+            var calculatedDistance = wordDistanceCalculator.Calculate(startWord, endWord);
+
+            Assert.Equal(expectedDistance, calculatedDistance);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("", null)]
+        [InlineData(null, null)]
+        [InlineData(null, "")]
+        [InlineData(null, "word")]
+        [InlineData("", "word")]
+        [InlineData("word", "")]
+        public void Calculate_Empty_Words_Throw_ArgumentException(string startWord, string endWord)
+        {
+            var wordDistanceCalculator = new WordDistanceCalculator();
+
+            Assert.Throws<ArgumentException>(() => wordDistanceCalculator.Calculate(startWord, endWord));
+        }
+
+        [Theory]
+        [InlineData("abc", "abcd")]
+        [InlineData("word", "wordd")]
+        public void Calculate_Different_Length_Words_Throw_ArgumentException(string startWord, string endWord)
+        {
+            var wordDistanceCalculator = new WordDistanceCalculator();
+
+            Assert.Throws<ArgumentException>(() => wordDistanceCalculator.Calculate(startWord, endWord));
+        }
+    }
+}
